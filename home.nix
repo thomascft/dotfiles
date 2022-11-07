2cb0204e1c7a2cc7704304a7657c14d2da42bf46
Thomas Croft <thomas.croft@proton.me>
2022-11-07 13:34:11 -0700

    Initial home.nix

---
+++ home.nix
@@ -0,0 +1,49 @@
+# This is your home-manager configuration file
+# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
+
+{ inputs, lib, config, pkgs, ... }: {
+  imports = [
+    # If you want to use home-manager modules from other flakes (such as nix-colors), use something like:
+    # inputs.nix-colors.homeManagerModule
+
+	nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    hyprland = {
+      url = "github:hyprwm/Hyprland";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    # Feel free to split up your configuration and import pieces of it here.
+  ];
+
+  home = {
+    username = "thomas";
+    homeDirectory = "/home/thomas";
+  };
+
+  # Add stuff for your user as you see fit:
+  programs.neovim.enable = true;
+  home.packages = with pkgs; [ 
+    #steam
+    wezterm
+
+	waybar
+	wlogout
+	swaylock-effects
+
+	firefox
+
+  ];
+
+  # Enable home-manager and git
+  programs.home-manager.enable = true;
+  programs.git.enable = true;
+
+  # Nicely reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "unstable";
+}
