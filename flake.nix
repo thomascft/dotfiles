a270735cb78b82e79f26688daf9adb576a0386f6
Thomas Croft <thomas.croft@proton.me>
2022-11-07 13:15:48 -0700

    Simple flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      hostname = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.thomas = import ./home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
