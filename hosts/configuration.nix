32f064d636487a39318242544028762f882a892f
Thomas Croft <103956335+thomascft@users.noreply.github.com>
2022-11-17 10:44:08 -0700

    Hyprland Cachix

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -66,6 +66,10 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+	settings = {
+	  substituters = ["https://hyprland.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+	};
   };
 
   system.stateVersion = "22.05";
