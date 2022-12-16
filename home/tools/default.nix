b9188a308a27e2a79960036eec04c0ed86cf8d01
Thomas Croft <103956335+thomascft@users.noreply.github.com>
2022-12-16 15:32:02 -0700

    github-cli for git auth

--- home/tools/default.nix
+++ home/tools/default.nix
@@ -15,6 +15,12 @@
       userName = "Thomas Croft";
       userEmail = "103956335+thomascft@users.noreply.github.com";
       lfs.enable = true;
+	  extraConfig = {
+	    credential = {
+          "https://github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+          "https://gist.github.com".helper = "!${pkgs.gh}/bin/gh auth git-credential";
+		}; 
+	  };
     };
   };
 }
